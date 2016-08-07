1f8c1381f9a4f11de036ad1e19f187d1ba66c62c
anhhh <anhhh.11@gmail.com>
2016-08-08 00:10:40 +0700

    Update

--- libs/agent.js
+++ libs/agent.js
@@ -9,6 +9,7 @@ import moment from 'moment'
  *  các nhân: initialState
  *  môi trường: environment
  * Sống - live
+ * Hoàn thành mục tiêu - go
  * Đạt được mục tiêu - isAchievedGoal
  */
 const log = console.log.bind(console);
@@ -16,57 +17,34 @@ export class Agent {
   constructor(environment = immutable.Map({}), initialState = immutable.Map({})) {
     this.state = initialState;
     this.environment = environment;
-    this.state = this.state.set('currentPosition', this.state.get('startPosition'));
   }
 
-  isGoalReached() {
-    return this.state.get('currentPosition') === this.environment.get('destination');
+  go(currentPosition, goal, path) {
+    if (currentPosition === goal) {
+      return path.push(goal);
+    }
+    const nextInformedPositions = this.environment.getIn(['maps', currentPosition]);
+    const prioritizedPositions = nextInformedPositions
+      .sortBy(function (distance, position) {
+        return position;
+      }, (positionA, positionB) => (positionA > positionB) ? -1 : 1);
+    for (let position of prioritizedPositions.keys()) {
+      let newPath = this.go(position, goal, path.push(currentPosition));
+      if (newPath) return newPath;
+    }
   }
 
   live() {
     var start = moment();
-    var alternatives = immutable.List();
-    var x = 0;
-    while (true) {
-      x++;
-      // Done on start
-      if (this.isGoalReached()) {
-        console.debug('Done on start');
-        break;
-      }
-      // Find ways & try
-      const currentPosition = this.state.get('currentPosition');
-      const nextInformedPositions = this.environment.getIn(['maps', currentPosition]);
-      const prioritizedPositions = nextInformedPositions
-        .sortBy(function (distance, position) {
-          return position;
-        }, (positionA, positionB) => (positionA > positionB) ? -1 : 1);
-      prioritizedPositions.forEach((distance, position) => {
-        alternatives = alternatives.push(position);
-      });
-      // No ways -> done
-      if (alternatives.count() == 0) {
-        console.debug('No way to try! Get stuck');
-        break;
-      }
-      // Has ways -> try
-      const alternative = alternatives.last();
-      alternatives = alternatives.pop();
-      this.state = this.state
-        .set('currentPosition', alternative);
-      // Try success
-      if (alternative === this.environment.get('destination')) {
-        console.debug('Done after trying');
-        break;
-      }
-      // Too long
-      if (x == 200) {
-        console.debug('Maybe loop forever');
-        break;
-      }
+    var path = this.go(this.state.get('startPosition'),
+      this.environment.get('destination'),
+      immutable.List());
+    if (path) {
+      console.debug(path.toJS());
+    } else {
+      console.debug('Not found');
     }
     var end = moment();
-    console.log('Path', this.state.get('path').toJS());
     console.debug(end.diff(start, 'mil'), 'mil');
   }
 
